a33174e498f875e15b85d6ad36d5dcc01aeac254
Trang1711 <tranglam17115@gmail.com>
2026-06-03 11:31:01 +0700

    delete folder

--- redmine-smart-ui/start-mac-linux.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-if [ ! -d node_modules ]; then
-  echo "Dang cai dat thu vien lan dau..."
-  npm install
-fi
-npm run dev
--- redmine-smart-ui/start-windows.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %~dp0
-if not exist node_modules (
-  echo Dang cai dat thu vien lan dau...
-  call npm install
-)
-call npm run dev
-pause
